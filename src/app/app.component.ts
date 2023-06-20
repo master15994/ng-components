c4c176520057f15cd8caa9d7dd7da6e153e0f3a4
Askar Pulatov <master15994@gmail.com>
2023-06-20 16:19:29 +0300

    forms&validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,54 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-export interface IPerson {
-  name: string;
-  height: string;
-  mass: string;
-  hair_color: string;
-  skin_color: string;
-  eye_color: string;
-  birth_year: string;
-  gender: string;
-  homeworld: string;
-  films: string[];
-  species: string[];
-  vehicles: string[];
-  starships: string[];
-  created: string;
-  edited: string;
-  url: string;
-}
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  todos: IPerson[] = [];
-
-  constructor(private http: HttpClient) {}
+  former!: FormGroup;
 
   ngOnInit(): void {
-    this.http
-      .get<IPerson[]>('https://swapi.dev/api/people/1')
-      .subscribe((res) => (this.todos = res));
+    this.former = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      address: new FormGroup({
+        country: new FormControl('by'),
+        city: new FormControl('', Validators.required),
+      }),
+      skills: new FormArray([], Validators.required),
+    });
+  }
+
+  sumbit() {
+    if (this.former.valid) {
+      console.log('ffffff', this.former);
+      const formData = { ...this.former.value };
+      console.log('form Data', formData);
+    }
+  }
+
+  setCity() {
+    const cityMap = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск',
+    };
+
+    const cityKey: 'ru' | 'ua' | 'by' = this.former
+      .get('address')
+      ?.get('country')?.value;
+    const city = cityMap[cityKey];
+
+    this.former.patchValue({ address: { city } });
+  }
+
+  addSkills() {
+    const control = new FormControl('', Validators.required);
+    (<FormArray>this.former.get('skills')).push(control);
   }
 }
